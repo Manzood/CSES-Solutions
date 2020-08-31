1ce45e88725ed0ac3eee12ef7ebcbe1f8c55bfa1
Manzood <manzood.naqvi@gmail.com>
2020-08-31 15:27:08 +0530

    Modified a problem, still haven't managed to solve it

--- Dynamic_Programming/coin_combinations_2.cpp
+++ Dynamic_Programming/coin_combinations_2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define debug(x) cout<<#x<<" = "<<x<<endl;
 #define sz(x) (int) (x).size()
 const int MOD = 1e9+7;
+// eventually, I will solve this problem properly
 
 int main() {
     int n, x;
@@ -11,23 +12,36 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &coins[i]);
     }
-    vector <long long> dp(x+1);
-    vector <set <int> > last(x+1);
+    sort(coins.begin(), coins.end());
+    vector <int> dp(x+1);
     dp[0] = 1;
-    last[0].insert(0);
+    vector <vector <int>> last(x+1);
     for (int i = 1; i <= x; i++) {
         dp[i] = 0;
-        for (auto c: coins) {
-            if (i - c >= 0) {
-                for (auto s: last[i-c]) {
-                    if (s <= c) {
-                        dp[i]++;
-                        last[i].insert(c);
+        for (int k = 0; k < n; k++) {
+            int c = coins[k];
+            last[i].push_back(0);
+            if (i - c > 0) {
+                for (int j = 0; j < n; j++) {
+                    if (coins[j] <= c) {
+                        dp[i] += last[i-c][j];
+                        last[i][k] += last[i-c][j];
                         dp[i] %= MOD;
+                        last[i][k] %= MOD;
+                    }
+                    else {
+                        break;
                     }
                 }
             }
+            else {
+                last[i][k] = 1;
+            }
+            // debug(i);
+            // debug(k);
+            // debug(coins[k]);
+            // debug(last[i][k]);
         }
     }
-    printf("%lld", dp[x]);
+    printf("%d\n", dp[x]);
 }
