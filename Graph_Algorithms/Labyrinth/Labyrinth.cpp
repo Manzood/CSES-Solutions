b58ba95b3a19942380c3ffd7d30202e83c805951
Manzood <manzood.naqvi@gmail.com>
2022-01-09 20:53:40 +0530

    Solved Labyrinth

--- Graph_Algorithms/Labyrinth/Labyrinth.cpp
+++ Graph_Algorithms/Labyrinth/Labyrinth.cpp
@@ -1,78 +1,112 @@
 #include "bits/stdc++.h"
+#ifdef local
+    #include "custom/debugger.h"
+#else
+    #define debug(x) 42;
+#endif
 using namespace std;
-#define debug(x) cout << #x << " = " << x << endl;
 #define int long long
 
-// dfs doesn't work, do a bfs
-
-bool ok (int x, int y, int n, int m) {
+vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+bool ok(int x, int y, int n, int m) {
     return x >= 0 && y >= 0 && x < n && y < m;
 }
-// shortest!!
 
-bool found = false;
-string shortest;
-string p; // path
+queue <pair <int, int>> q;
 vector <vector <bool>> visited;
-void find_path (vector <string>& grid, int x, int y, int n, int m) {
-    // print only if !found
-    // search right
-    if (visited[x][y] || found) return;
-    visited[x][y] = true;
-    if (grid[x][y] == 'B') {
-        if (!found) {
-            // cout << "YES\n";
-            // cout << p.size() << "\n";
-            // cout << p << "\n";
-            shortest = p;
+
+bool bfs (int start_i, int start_j, vector <string>& grid, vector <string>& prev) {
+    int n = grid.size();
+    int m = grid[0].size();
+    bool found = false;
+    // nonrecursive
+    int i = start_i;
+    int j = start_j;
+    visited[i][j] = true;
+
+    while (1) {
+        if (grid[i][j] == 'B') {
             found = true;
-        } else {
-            if (p.size() < shortest.size()) shortest = p;
+            break;
         }
+        for (auto x: dirs) {
+            if (ok(i + x.first, j + x.second, n, m)) {
+                if (!visited[i+x.first][j+x.second] && grid[i+x.first][j+x.second] != '#') {
+                    if (x.first == 1 && x.second == 0) {
+                        prev[i+x.first][j+x.second] = 'D';
+                    }
+                    if (x.first == -1 && x.second == 0) {
+                        prev[i+x.first][j+x.second] = 'U';
+                    }
+                    if (x.first == 0 && x.second == 1) {
+                        prev[i+x.first][j+x.second] = 'R';
+                    }
+                    if (x.first == 0 && x.second == -1) {
+                        prev[i+x.first][j+x.second] = 'L';
+                    }
+                    q.push ({i + x.first, j + x.second});
+                    visited[i+x.first][j+x.second] = true;
+                }
+            }
+        }
+        if (q.empty()) {
+            break;
+        }
+        i = q.front().first;
+        j = q.front().second;
+        q.pop();
+        // how do I get the path?
     }
-    if (ok (x, y + 1, n, m) && grid[x][y+1] != '#') {
-        p += 'R';
-        find_path (grid, x, y + 1, n, m);
-        p.pop_back();
-    }
-    if (ok (x + 1, y, n, m) && grid[x+1][y] != '#') {
-        p += 'D';
-        find_path (grid, x + 1, y, n, m);
-        p.pop_back();
-    }
-    if (ok (x, y - 1, n, m) && grid[x][y-1] != '#') {
-        p += 'L';
-        find_path (grid, x, y - 1, n, m);
-        p.pop_back();
-    }
-    if (ok (x - 1, y, n, m) && grid[x-1][y] != '#') {
-        p += 'U';
-        find_path (grid, x - 1, y, n, m);
-        p.pop_back();
-    }
+
+    return found;
 }
 
-int32_t main () {
+int32_t main() {
     int n, m;
     scanf("%lld%lld", &n, &m);
     vector <string> grid(n);
+    vector <string> prev(n);
+    visited.resize(n, vector <bool> (m, false));
+    int startx = -1, starty = -1;
+    int endx = -1, endy = -1;
     for (int i = 0; i < n; i++) {
         cin >> grid[i];
-    }
-    visited.resize (n, vector <bool> (m, false));
-    // start the search at A
-    for (int i = 0; i < n; i++) {
+        prev[i].resize(m, '.');
         for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 'A') {
-                find_path (grid, i, j, n, m);
+            char c = grid[i][j];
+            if (c == 'A') {
+                startx = i;
+                starty = j;
+            } else if (c == 'B') {
+                endx = i;
+                endy = j;
             }
         }
     }
+
+    bool found = bfs (startx, starty, grid, prev);
     if (!found) {
-        cout << "NO\n";
+        printf("NO\n");
     } else {
-        cout << "YES\n";
-        cout << shortest.size() << "\n";
-        cout << shortest << "\n";
+        printf("YES\n");
+        // find path
+        int i = endx;
+        int j = endy;
+        string path;
+        while (grid[i][j] != 'A') {
+            path.push_back (prev[i][j]);
+            if (prev[i][j] == 'L') {
+                j++;
+            } else if (prev[i][j] == 'R') {
+                j--;
+            } else if (prev[i][j] == 'D') {
+                i--;
+            } else if (prev[i][j] == 'U') {
+                i++;
+            }
+        }
+        reverse (path.begin(), path.end());
+        printf("%lld\n", (int) path.size());
+        cout << path << "\n";
     }
 }
