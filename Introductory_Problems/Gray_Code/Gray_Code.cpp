9cc79768623b03aa3fa09b7854f6f45b16f0f05c
Manzood <manzood.naqvi@gmail.com>
2022-01-27 12:27:09 +0530

    Solved Gray Code

--- Introductory_Problems/Gray_Code/Gray_Code.cpp
+++ Introductory_Problems/Gray_Code/Gray_Code.cpp
@@ -5,21 +5,36 @@ using namespace std;
 
 string code;
 
+vector <string> codes;
 void make_code (int n) {
-    if (!n) {
-        cout << code << "\n";
-        return;
+    if (n == 0) return;
+    int len = (int) codes.size();
+    vector <string> bottom = codes;
+    for (int i = 0; i < len; i++) {
+        reverse(codes[i].begin(), codes[i].end());
+        codes[i] += '0';
     }
-    code += "0";
+    reverse(bottom.begin(), bottom.end());
+    for (int i = 0; i < len; i++) {
+        reverse(bottom[i].begin(), bottom[i].end());
+        bottom[i] += '1';
+    }
+    for (int i = 0; i < len; i++) {
+        codes.push_back(bottom[i]);
+    }
+    for (int i = 0; i < 2 * len; i++) {
+        reverse(codes[i].begin(), codes[i].end());
+    }
     make_code (n - 1);
-    code.pop_back();
-    code += "1";
-    make_code (n - 1);
-    code.pop_back();
 }
 
 int32_t main () {
     int n;
     scanf("%lld", &n);
-    make_code (n);
+    codes.push_back("0");
+    codes.push_back("1");
+    make_code (n - 1);
+    for (int i = 0; i < (int) codes.size(); i++) {
+        cout << codes[i] << "\n";
+    }
 }
