da95d78f98fb2ae443f083a2855bd752b8bf3036
Manzood <manzood.naqvi@gmail.com>
2022-12-10 14:21:26 +0530

    Solved another problem

--- Sorting_and_Searching/RoomAllocation/RoomAllocation.cpp
+++ Sorting_and_Searching/RoomAllocation/RoomAllocation.cpp
@@ -12,19 +12,37 @@ using namespace std;
 void solve([[maybe_unused]] int test) {
     int n;
     scanf("%lld", &n);
-    vector<int> a(n), b(n);
+    vector<pair<int, int>> start(n), end(n);
     for (int i = 0; i < n; i++) {
-        scanf("%lld%lld", &a[i], &b[i]);
+        scanf("%lld%lld", &start[i].first, &end[i].first);
+        start[i].second = i;
+        end[i].second = i;
     }
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    int j = 0;
-    int mx = 0;
-    vector<int> ans(n);
-    // find mex??
+    vector<int> f(n);
+    iota(f.begin(), f.end(), 0);
+    reverse(f.begin(), f.end());
+    sort(start.begin(), start.end());
+    sort(end.begin(), end.end());
+    int cnt = 0, tot = 0;
+    int s = 0, e = 0;
+    vector<int> ans(n), assigned(n, -1);
+    while (s < n) {
+        if (start[s].first <= end[e].first) {
+            // new room
+            cnt++;
+            ans[start[s].second] = f.back();
+            f.pop_back();
+            s++;
+        } else {
+            cnt--;
+            f.push_back(ans[end[e].second]);
+            e++;
+        }
+        tot = max(cnt, tot);
+    }
+    printf("%lld\n", tot);
     for (int i = 0; i < n; i++) {
-        while (j < n && b[j] < a[i]) j++;
-        int req = i - j + 1;
+        printf("%lld ", ans[i] + 1);
     }
 }
 
